c19434d5f538637516d535ebdc73000fe75865d5
Nathan Spencer <nathantspencer@gmail.com>
2022-10-24 17:20:41 -0400

    Adds functions for calculating total length

--- swick/swc.py
+++ swick/swc.py
@@ -13,3 +13,18 @@ class SWC:
 
     def __init__(self, trees: list[Tree]):
         self.trees = trees
+
+    def total_length(self):
+        """
+        Calculates and returns the sum of the Euclidean distances between each
+        pair of connected nodes in each of the trees owned by this ``SWC``.
+
+        :return:
+            the sum of the distances between each pair of connected nodes in
+            each tree
+        """
+
+        total_length = 0.0
+        for tree in self.trees:
+            total_length += tree.total_length()
+        return total_length
