4ac918f4dcc785472fca5f42badb5b1ab13e88a4
Nathan Spencer <nathantspencer@gmail.com>
2024-08-30 11:54:24 -0400

    Fix python_requires syntax, increment version to 0.29

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name="swick",
-    version="0.28",
+    version="0.29",
     description="The slick way to process SWC files.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="Nathan T. Spencer",
     author_email="nathantspencer@gmail.com",
     url="https://nathantspencer.com",
-    python_requires=">=3.**",
+    python_requires=">=3.0",
     install_requires=[''],
     packages=['swick'],
     package_data={
